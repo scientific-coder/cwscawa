cebdbe10644f6d18f3174ab33fb083b34ff66e9b
scientific-coder <public-code@bernard-hugueney.org>
2012-06-08 08:50:42 +0200

    better json format

--- src/cwscawa/server.clj
+++ src/cwscawa/server.clj
@@ -10,7 +10,6 @@
 
 (def workers (ref {}))
 (defn add-body [handler]
-
   (fn [req]
     ;; "text/text"
     (do (println req)
@@ -21,27 +20,20 @@
                    req)]
 
         (handler neue)))))
+
 (server/add-middleware add-body)
 
-;;; added this to my view file
 (defn add-wks[current-wks new-wks]
-  (do
-    (println "the current in add-wks: " current-wks)
-;;    current-wks
-    (reduce #(merge %1
-                    {(keyword (%2 :id))
-                     (-> %2
-                         (dissoc :id)
-                         (assoc :load 0
-                                :last-jobs []))})
-            current-wks
-            new-wks)))
+  (letfn [(empty-wk [[id prop]]
+            {id (merge prop {:load 0 :last-jobs []})})]
+    (reduce #(merge %1 (empty-wk %2)) current-wks new-wks)))
 
 ;;; curl -d @/home/bernard/Code/repositories/cwscawa/test/cwscawa/new-workers.json --header "Content-Type: application/json"  http://localhost:8080/workers/add
 (defpage [:post "/workers/add"] {:as params}
-(do (dosync (alter workers #(add-wks % (:backbone params))))
+  (do (dosync (alter workers #(add-wks % (:backbone params))))
       (println "new workers: " (:backbone params) "all workers: " @workers)
       (response/json @workers )))
+
 (defn -main [& m]
   (let [mode (keyword (or (first m) :dev))
         port (Integer. (get (System/getenv) "PORT" "8080"))]
