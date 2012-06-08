1760fb6edb7806f05a45fdca3cbf1a9e694481b7
scientific-coder <public-code@bernard-hugueney.org>
2012-06-08 15:45:32 +0200

    trying Marginalia (but it currently fails at lein marg).

--- project.clj
+++ project.clj
@@ -3,5 +3,6 @@
             :dependencies [[org.clojure/clojure "1.4.0"]
                            [noir "1.3.0-beta3"]
                            [cheshire "2.0.2"]]
+            :plugins [[lein-marginalia "0.7.0"]]
             :main cwscawa.server)
 
