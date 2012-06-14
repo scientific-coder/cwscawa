c64f1996f3c426405254f41aa9b9eaced1e8c8cd
scientific-coder <public-code@bernard-hugueney.org>
2012-06-14 15:06:19 +0200

    new marginalia version fixes the bug. lein marg now works.

--- project.clj
+++ project.clj
@@ -4,6 +4,6 @@
                            [org.clojure/data.priority-map "0.0.1"]
                            [noir "1.3.0-beta3"]
                            [cheshire "2.0.2"]]
-            :plugins [[lein-marginalia "0.7.0"]]
+            :plugins [[lein-marginalia "0.7.1"]]
             :main cwscawa.server)
 
